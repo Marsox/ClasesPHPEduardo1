ee3b14d556ebcd5327eed525e2ce69db9f23b0ba
Unknown <eduarmarso@hotmail.com>
2018-01-24 13:19:17 +0100

    Incluido Select Categoria
    El Select muestra todas las categorias.

--- paginas/contacto/nuevoContacto.php
+++ paginas/contacto/nuevoContacto.php
@@ -1,3 +1,10 @@
+<?php 
+include_once '../../lib.php';
+include_once '../../modelo/categoria.php';
+session_start();
+
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,32 +12,48 @@
 	<title>NUevo Contacto</title>
 </head>
 <body>
-	<form action="../../acciones/contacto/crear.php" method="POST">
-		<p>
-			<label for="nombre"> nombre </label>
-			<input type="text" name="nombre">
-		</p>
-		<p>
-			<label for="apellido"> apellido </label>
-			<input type="text" name="apellido">
-		</p>
-		<p>
-			<label for="telefono"> telefono </label>
-			<input type="text" name="telefono">
-		</p>
-		<p>
-			<label for="email"> email </label>
-			<input type="email" name="email">
-		</p>
-		<p>
-			<label for="direccion"> direccion </label>
-			<input type="text" name="direccion">
-		</p>
-		<p>
-			<label for="categoria"> categoria </label>
-			<input type="text" name="categoria">
-		</p>
-		<input type="submit" value="Crear">
-	</form>
+	<?php if (hayErrores()) {
+		mostrarErrores();
+	}
+
+	if (isset($_SESSION['user'])) {
+		$categorias = Categoria::all();
+
+	?>
+		<form action="../../acciones/contacto/crear.php" method="POST">
+			<p>
+				<label for="nombre"> nombre </label>
+				<input type="text" name="nombre">
+			</p>
+			<p>
+				<label for="apellido"> apellido </label>
+				<input type="text" name="apellido">
+			</p>
+			<p>
+				<label for="telefono"> telefono </label>
+				<input type="text" name="telefono">
+			</p>
+			<p>
+				<label for="email"> email </label>
+				<input type="email" name="email">
+			</p>
+			<p>
+				<label for="direccion"> direccion </label>
+				<input type="text" name="direccion">
+			</p>
+			<p>
+				<label for="categoria"> categoria </label>
+				<select name="categoria">
+					<?php foreach ($categorias as $categoria) {
+						echo "<option value=\"".$categoria->getId()."\">". $categoria->getNombre() ."</option>";
+					} ?>
+				</select>
+			</p>
+			<input type="submit" value="Crear">
+		</form>
+	<?php
+	}else{
+		header('Location: ../usuario/login.php');
+	} ?>
 </body>
 </html>
